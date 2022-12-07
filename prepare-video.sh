475ae5c257aecb1afda90eb08b58d0e8e035ae17
rafawalter <rafawalter@gmail.com>
2022-12-07 07:26:21 -0300

    prepare in low res

--- prepare-video.sh
+++ prepare-video.sh
@@ -2,5 +2,7 @@ script_dir=$(dirname $0)
 
 time (
 	$script_dir/metadata/mark-as-photosphere.sh $1
-	$script_dir/encode/encode-all.sh $1
+	# $script_dir/encode/encode-all.sh $1
+	$script_dir/encode/encode-790.sh $1
+	$script_dir/encode/encode-1920.sh $1
 )
